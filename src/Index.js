84b2289fa66bb81a04e9f7b400a75826ffc8004c
pascui <francopascua7@gmail.com>
2025-09-02 20:25:18 -0300

    first commit

---
+++ src/Index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import port from "./configs.js";
+import { sequelize } from "./db.js";
+import hotelRoutes from "./routes/hotel.routes.js";
+
+import "./models/hotel.js";
+
+const app = express();
+
+try {
+	app.listen(port);
+	app.use(hotelRoutes);
+	await sequelize.sync();
+	console.log("app listening in port ", port);
+} catch (error) {
+	console.log("there was an error on initilization");
+}
